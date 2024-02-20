fb75602b8a3a3bda343ec0d85ea9918d2eab77e2
watakak <155397402+watakak@users.noreply.github.com>
2024-02-20 02:35:51 +0100

    Create cpp_code.py

---
+++ compilers/cp/cpp_code.py
@@ -0,0 +1,29 @@
+import re
+
+#C% COMPILER INTO C++ 1.0 STABLE VERSION.
+
+input_file = 'compilers/main.cp'
+
+replacement_dict = {
+    r'console.out\(\'(.*?)\'\)': r'std::cout << "\1" << std::endl;',
+    'include cp': '#include <iostream>',
+    'return class': 'return 0;',
+    'class': 'int',
+    'def': 'void',
+}
+
+def compile():
+    print('cp -code main.cp -cpp\n')
+    print(modified_code)
+
+def replace_print_with_console_out(code):
+    for pattern, replacement in replacement_dict.items():
+        code = re.sub(pattern, replacement, code)
+    return code
+
+with open(input_file, 'r') as file:
+    code = file.read()
+
+modified_code = replace_print_with_console_out(code)
+
+compile()
